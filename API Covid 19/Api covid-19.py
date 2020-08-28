d2a821ed7d978ffb59d438b532839631636ca18c
Rafael Gallo <rafaelhenriquegallo@gmail.com>
2020-08-28 20:50:03 -0300

    Update Api covid-19.py

--- API Covid 19/Api covid-19.py
+++ API Covid 19/Api covid-19.py
@@ -1,46 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # API Covid-19 
-
-# In[7]:
-
-
 import json
 import requests
-
 import pandas as pd
 import seaborn as sns
-
-
-# In[8]:
 
 
 url = "https://covid19-brazil-api.now.sh/api/report/v1" 
 
 headers = {
     'x-rapidapi-host': "covid19-brazil-api.now.sh/api/report/v1",
-    'x-rapidapi-key': "9df309daf7msh4afaa54184a9cc4p1801d1jsn0feb78294818"
+    'x-rapidapi-key': "KEY"
     }
 
 response = requests.request("GET", url, headers=headers)
 print(response)
 
-
-# In[9]:
-
-
 parsed_data = json.loads(response.text)
 print(parsed_data)
-
-
-# In[10]:
-
-
 parsed_data
-
-
-# In[11]:
 
 
 def flatten_json(json):
@@ -56,18 +32,6 @@ def flatten_json(json):
     flatten(json)
     return dict1
 
-
-# In[12]:
-
-
 #Data Frame
-
 dados = pd.DataFrame.from_dict(flatten_json(parsed_data),orient="index")
 dados
-
-
-# In[ ]:
-
-
-
-
